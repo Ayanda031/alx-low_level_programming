1bb4462760b87c68a3f2de096cbcb2c3125d11cd
Ayanda Mnguni <Ayandaayoba19@gmail.com>
2023-02-26 12:37:09 -0800

    the success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,18 @@ int main(void)
 		{
 		for (q = p + 1; q <= '9'; q++)
 		{
+		if ((p != d) != q)
+		{
+			putchar(d);
+			putchar(p);
+			putchar(q);
 		if (d == '7' && p == '8')
 			continue;
 		putchar(',');
 		putchar(' ');
 		}
 		}
+	}
 	}
 	putchar('\n');
 	return (0);

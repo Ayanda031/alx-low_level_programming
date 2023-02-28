0c7848b3e938df874dfa5da722fbd9b294ae0565
Ayanda Mnguni <Ayandaayoba19@gmail.com>
2023-02-28 00:10:29 -0800

    98 battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * reset_to_98 - update the value of a var using pointers
  * @n: int to update
  * Return: 0
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 
 {
 
-	*n = 98;
-	return (0);
+	(*n = 98);
+
 }
